08af3348767b00dd4965db6d0f9a32498a80ad32
martimfasantos <marty.f.a.santos@gmail.com>
2024-01-24 17:27:57 +0100

    changes after second submission ; new approach, encodings and models to train

--- eval_aux.py
+++ eval_aux.py
@@ -90,55 +90,86 @@ for i in range(len(file_names)):
                     shuffle=True,
                     random_state=3)
     
+    # Here, we want to find the best classifier. As candidates, we consider
+    #   1. LogisticRegression
+    #   2. RandomForestClassifier
+    #   3. GradientBoostingClassifier
+    #   4. HistGradientBoostingClassifier
+    #   5. AdaBoostClassifier
+    #   6. MLPClassifier
+        
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import GradientBoostingClassifier, HistGradientBoostingClassifier, AdaBoostClassifier
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.decomposition import PCA
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.model_selection import GridSearchCV
+
+    # Classifiers
     model_logistic_regression = LogisticRegression(max_iter=300)
     model_random_forest = RandomForestClassifier()
     model_gradient_boosting = GradientBoostingClassifier()
+    model_adaboost = AdaBoostClassifier()
+    model_hist_gradient_boosting = HistGradientBoostingClassifier()
     model_neural_network = MLPClassifier(max_iter=200)
-    
+
     # train the models
-    if FLAG == 'missing_values' or FLAG == 'outliers':
-        pipeline = Pipeline(steps=[("scaler", transform_scaler), 
-                                ("pca", transform_pca),
-                                ("model", None)])
-    else:
-        pipeline = Pipeline(steps=[("pca", transform_pca),
-                                ("model", None)])
+    pipeline = Pipeline(steps=[("pca", PCA()),
+                            ("model", None)])
 
     parameter_grid_preprocessing = {
-    # "pca__n_components" : [1, 2, 3, 4],
+        # "pca__n_components" : [3, 4, 5],
         "pca__n_components" : [df.shape[1]-12, df.shape[1]-8, df.shape[1]-5, df.shape[1]]
     }
 
-    # parameter_grid_logistic_regression = {
-    #   "model" : [model_logistic_regression],
-    #   "model__C" : [0.1, 1, 10],  # inverse regularization strength
-    # }
+    # NOTE: Logistic Regression does not perform as well as the other models
+    parameter_grid_logistic_regression = {
+        "model" : [model_logistic_regression],
+        "model__C" : [0.1, 1, 10],  # inverse regularization strength
+    }
 
     parameter_grid_gradient_boosting = {
         "model" : [model_gradient_boosting],
-        "model__n_estimators" : [20, 30, 50]
+        "model__learning_rate" : [0.2], 
+        "model__max_depth" : [4],
+        "model__n_estimators" : [300]
     }
 
+    # This estimator is much faster than GradientBoostingClassifier for big datasets (n_samples >= 10 000).
+    parameter_grid_hist_gradient_boosting = {
+        "model" : [model_hist_gradient_boosting],
+        "model__learning_rate" : [0.2], 
+        "model__max_depth" : [4, 5, 6],
+    }
+
+    parameter_grid_adaboost = {
+        "model" : [model_adaboost],
+        "model__n_estimators" : [150]
+    }
+    
     parameter_grid_random_forest = {
         "model" : [model_random_forest],
         "model__n_estimators" : [30, 50, 75],  # number of max trees in the forest
-        # "model__max_depth" : [2, 3, 4],
-        "model__max_depth" : [20, df.shape[1]],
+        "model__max_depth" : [3, 4, 5],
+        # "model__max_depth" : [10, 15, 20, df.shape[1]],
     }
 
     # NOTE: NN does not perform well on this dataset + takes a long time to train
-    # parameter_grid_neural_network = {
-    #     "model": [model_neural_network],
-    #     "model__hidden_layer_sizes": [(30, 30), (40, 30)],  # Example hidden layer configurations
-    #     "model__alpha": [0.0001],  # Regularization parameter
-    # }
-    
-    
+    parameter_grid_neural_network = {
+        "model": [model_neural_network],
+        "model__hidden_layer_sizes": [(30, 30), (40, 30)],  # Example hidden layer configurations
+        "model__alpha": [0.0001],  # Regularization parameter
+    }
+
     meta_parameter_grid = [
-                    # parameter_grid_logistic_regression,
-                       parameter_grid_random_forest,
-                       parameter_grid_gradient_boosting] #,
-                      #  parameter_grid_neural_network]
+                        # parameter_grid_logistic_regression,
+                        #  parameter_grid_random_forest] #,
+                        #  parameter_grid_gradient_boosting] #,
+                        parameter_grid_hist_gradient_boosting] #,
+                        #  parameter_grid_adaboost ] #,
+                        #  parameter_grid_neural_network]
 
     meta_parameter_grid = [{**parameter_grid_preprocessing, **model_grid}
                         for model_grid in meta_parameter_grid]
@@ -146,7 +177,6 @@ for i in range(len(file_names)):
     search = GridSearchCV(pipeline,
                         meta_parameter_grid, 
                         scoring="balanced_accuracy",
-                        # n_jobs=2, 
                         n_jobs=-1, 
                         cv=5,  # number of folds for cross-validation 
                         error_score="raise"
@@ -155,7 +185,7 @@ for i in range(len(file_names)):
     # here, the actual training and grid search happens
     search.fit(X_train, y_train.ravel())
 
-    print(f"{file_names[i]}: train best parameters:", search.best_params_ ,"(CV score=%0.3f)" % search.best_score_)
+    print("best parameter:", search.best_params_ ,"(CV score=%0.3f)" % search.best_score_)
     print("-" * 20)
     
     # evaluate performance of model on test set
